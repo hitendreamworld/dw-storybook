b73404f608b514f89984fbfbbe512c8ce9047c37
Hiten Bhayani <hiten@hisab.co>
2022-09-13 16:58:41 +0530

    feat: storybook integration

---
+++ stories/DwSurface.stories.js
@@ -0,0 +1,21 @@
+import { html } from "lit-html";
+import "@dreamworld/dw-surface";
+
+export default {
+  title: "Dreamworld/Surface",
+  component: "dw-surface",
+  argTypes: {
+    elevation: {
+      control: "select",
+      options: [0, 1, 2, 4, 8, 16, 24],
+    },
+  },
+};
+
+const Template = ({ elevation }) =>
+  html`<dw-surface .elevation=${elevation}>Elevation: ${elevation}</dw-surface>`;
+
+export const Default = Template.bind({});
+Default.args = {
+  elevation: 0,
+};
